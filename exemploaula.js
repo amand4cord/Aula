2d088df7f6bb073c913e7277953fcf99d3fcb62a
AMANDA DA SILVA CORDEIRO <amanda_da-cordeiro@estudante.sesisenai.org.br>
2025-05-05 21:27:26 -0300

    Exercicio 2,3 e 5

---
+++ exemploaula.js
@@ -0,0 +1,11 @@
+//estrutura de repetição while
+let contador = 1
+while(contador <= 10){
+    let multiplicando = 1
+    while(multiplicando >=10){
+        let resultado = multiplicador * multiplicando
+        alert("Resultado: " + resultado)
+        multiplicando++
+    }
+}
+multiplicador++
