f363b106b83ef5dd006da80531f716214c112826
AMANDA DA SILVA CORDEIRO <amanda_da-cordeiro@estudante.sesisenai.org.br>
2025-05-08 21:59:54 -0300

    Atulização

--- exemploaula.js
+++ exemploaula.js
@@ -9,3 +9,25 @@ while(contador <= 10){
     }
 }
 multiplicador++
+
+ // QUESTÃO 6 da lista de exercicios while
+ // gerar contagem ate 100
+ //somar os numeros
+ //parar se deu ruim
+ //mostrar o resultado
+
+ function somar(){
+    let soma = 0
+
+    let cont = 1     //inicializaçao da variavel de controle
+    while(cont <= 100 && soma <= 400){    // setar a condiçao de repetiçao
+        soma =  soma + cont
+        console.log("Cont: " + cont);
+        console.log("Soma: " + soma);
+        console.log("==================");
+
+        cont++   // modificaçao da contagem
+    }
+  console.log(" Soma: " + soma);
+
+}
